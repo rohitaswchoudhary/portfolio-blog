51d8b0ec9e2e6ad8ea2e6f22332910cfcce43046
rohitaswchoudhary <rohitaswchoudhary@gmail.com>
2021-09-18 16:06:10 +0530

    09

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')), # new
     path('', include('blog.urls')), # new
 
 ]
