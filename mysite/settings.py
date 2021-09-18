5cbf31f276ab6ac16acff683e93a193cbd498f47
rohitaswchoudhary <rohitaswchoudhary@gmail.com>
2021-09-18 15:15:57 +0530

    04

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,21 +78,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        # 'USER': 'postgres',
-        # 'PASSWORD': 'Rohitasw@2002',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-}    
+}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         # 'USER': 'postgres',
+#         # 'PASSWORD': 'Rohitasw@2002',
+#     }
+# }    
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
